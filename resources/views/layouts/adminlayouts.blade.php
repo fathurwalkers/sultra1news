13da2b02df26257882029ffbe11cc0387c01892d
fathurwalkers <fathurwalkers44@gmail.com>
2020-08-31 09:41:15 +0800

    staging 1

--- resources/views/layouts/adminlayouts.blade.php
+++ resources/views/layouts/adminlayouts.blade.php
@@ -175,7 +175,7 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="../assets/js/stisla.js"></script>
+    <script src="{{ asset('assets/stisla/js/stisla.js') }}"></script>
 
     <!-- JS Libraies -->
 
