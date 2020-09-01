b426eb750975b5ece859cc8509592612246de392
fathurwalkers <fathurwalkers44@gmail.com>
2020-09-02 07:50:16 +0800

    implementasi soft deletes

--- app/Article.php
+++ app/Article.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'post_judul',
         'post_isi',
