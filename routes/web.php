c2ebdc8632ce9e3da447c43a20856ad7ac074c7f
fathurwalkers <fathurwalkers44@gmail.com>
2020-09-21 20:50:31 +0800

    delete default route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 // Home Route
 Route::get('/', 'HomeController@index');
 Route::get('/login', 'AdminController@login');
